fe34a76c8e669c8803e91255f3f4814d822dd8bf
primaam <pmaharyono@gmail.com>
2025-02-14 13:58:28 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
-import "./App.css";
+import Typography from "@mui/material/Typography";
+import { AppBar, Container } from "@mui/material";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <div>dsdsd</div>
+            <AppBar position="static">
+                <h1>dhsjdhjs</h1>
+            </AppBar>
+            <p>dsdsd</p>
+            <Typography
+                variant="h1"
+                color="secondary"
+                onClick={() => console.log("test on click")}
+                sx={{ color: "skyblue" }}
+            >
+                test tulisan
+            </Typography>
+
+            <Container>
+                <div></div>
+            </Container>
         </>
     );
 }
